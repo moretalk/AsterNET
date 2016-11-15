90430c2149304cf09969c0c3441d2553426882df
Matthew Duggan <matthew@moretalk.uk.com>
2016-11-15 14:06:56 +0000

    Add Sip Notify Action

---
+++ Asterisk.2013/Asterisk.NET/Manager/Action/SipNotifyAction.cs
@@ -0,0 +1,39 @@
+﻿namespace AsterNET.Manager.Action
+{
+    public class SipNotifyAction : ManagerAction
+    {
+        /// <summary>
+        ///Sip Notify.
+        /// </summary>
+        public SipNotifyAction()
+        {
+
+        }
+
+        /// <summary>
+        ///     Sip Notify.
+        /// </summary>
+        /// <param name="channel">Transferer's channel.</param>
+        /// <param name="variable">Notify Variable</param>
+        public SipNotifyAction(string channel, string variable)
+        {
+            Channel = channel;
+            Variable = variable;
+        }
+
+        public override string Action
+        {
+            get { return "SIPnotify"; }
+        }
+
+        /// <summary>
+        ///     Sip Notify Destination channel.
+        /// </summary>
+        public string Channel { get; set; }
+
+        /// <summary>
+        ///     Sip Notify Variable name=
+        /// </summary>
+        public string Variable { get; set; }
+    }
+}
